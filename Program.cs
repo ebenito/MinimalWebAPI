8aba92bcd46fd40b1ce64bb8072a90df828b8e89
ebenito <juan_benito@artyco.com>
2024-11-19 11:13:01 +0100

    Actualizar endpoint para obtener IP real del usuario

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 //Demo conversión de la plantilla a una Web API minima
 using MinimalWebAPI;
+using Microsoft.AspNetCore.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -8,5 +9,19 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/api/greet/{name}", (string name) => $"Hello, {name}!");
 app.MapGet("/weatherforecast", () => WeatherForecastService.GetForecasts());
 
-app.Run();
+// Nuevo endpoint para obtener la IP del usuario
+app.MapGet("/api/get-ip", (HttpContext httpContext) =>
+{
+	var ipAddress = httpContext.Connection.RemoteIpAddress?.ToString();
 
+	// Verificar encabezados para obtener IP detrás de proxies
+	if (httpContext.Request.Headers.ContainsKey("X-Forwarded-For"))
+	{
+		ipAddress = httpContext.Request.Headers["X-Forwarded-For"].FirstOrDefault();
+	}
+
+	return ipAddress ?? "IP desconocida";
+});
+
+
+app.Run();
